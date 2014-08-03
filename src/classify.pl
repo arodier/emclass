2d4130e0db5b79a2a50b7142b2f22477deed1cbc
André Rodier <andre.rodier@gmail.com>
2014-08-03 20:25:51 +0100

    Updated ignore list for vim

--- src/classify.pl
+++ src/classify.pl
@@ -61,14 +61,12 @@ sub new {
 
     @recips         = $msginfo->recips;
 
-    # only work for me ATM
-    # exit unless $sender =~ /rodier/;
-
     # Check internal email first
     ($user,$domain) = split(/@/, $sender);
     $internal = scalar grep /$domain/, @local_domains_acl;
     if ( $internal ) {
       $type_value = 'Internal';
+      $reason = 'Same Domain';
       die 'Ok';
     }
 
